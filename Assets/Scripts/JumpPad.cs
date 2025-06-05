871830e04725c403fd117f28ebee89ebfd44d263
JPiggyG <jgilelach@hotmail.com>
2025-06-04 22:14:57 -0400

    Misc changes
    Changed some prefabs, reorganized the old levels and objects in certain scenes, and added one new level

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -7,7 +7,7 @@ public class JumpPadScript : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 12 && collision.gameObject.GetComponent<Rigidbody2D>().linearVelocityY < 0)
+        if (collision.gameObject.layer == 12 && collision.gameObject.GetComponent<Rigidbody2D>().linearVelocityY <= 0)
         {
             collision.gameObject.GetComponent<Rigidbody2D>().linearVelocityY = 0;
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
