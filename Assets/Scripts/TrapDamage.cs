4b0309f309092cc7de9f4cce3dba25e215f24552
jmacjz <zielinskijohn626@gmail.com>
2025-07-21 15:02:48 -0400

    Arrow now breaks on walls

--- Assets/Scripts/TrapDamage.cs
+++ Assets/Scripts/TrapDamage.cs
@@ -7,11 +7,15 @@ public class TrapDamage : MonoBehaviour
 
     private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
+        if (collision.gameObject.layer == 7)
+        {
+            Destroy(gameObject);
+        }
         if (collision.tag == "Player")
         {
             collision.GetComponent<PlayerScript>().TakeDamage(damage);
             GameObject.Find("Mirrored Player").GetComponent<MirroredPlayer>().TakeDamage(damage);
-            
+
         }
 
         else if (collision.tag == "Mirrored Player")
@@ -19,6 +23,8 @@ public class TrapDamage : MonoBehaviour
             collision.GetComponent<MirroredPlayer>().TakeDamage(damage);
             GameObject.Find("Player").GetComponent<PlayerScript>().TakeDamage(damage);
         }
+
         
+
     }
 }
